9295af9c13d807bd854985acada15502db063136
Hassam Ali <hassam-ali1@outlook.com>
2019-07-11 14:29:57 +0500

    React Tutorial

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';   
+import { format } from 'url';
 
 class Counter extends Component {
+    state = {
+        count: 1,
+        tags: ['tag1', 'tag2', 'tag3']
+    };
     render() { 
         return (
             <React.Fragment>
-                <h1>H3llo World</h1>
-                <button>Increment</button>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+                <ul>
+                    { this.state.tags.map(tag => <li key={tag}>{tag}</li>) }
+                </ul>
             </React.Fragment>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? 'warning' : 'primary';
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'zero' : count;
     }
 }
  
